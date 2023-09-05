262decef076e342107167802c912aeba1b0ee03d
AndreiChertkov <andre.chertkov@gmail.com>
2023-09-05 20:35:01 +0300

    Update zhores script

--- zhores_run.sh
+++ zhores_run.sh
@@ -8,7 +8,7 @@
 # conda activate && conda remove --name tetradat --all -y
 # conda create --name tetradat python=3.8 -y
 # conda activate tetradat
-# pip install "jax[cpu]==0.4.6" optax teneva==0.14.1 protes==0.3.1 torch torchvision matplotlib requests urllib3 torchattacks==3.4.0
+# pip install jupyterlab teneva_opti==0.4.2 torch torchvision matplotlib requests urllib3 torchattacks==3.4.0
 
 
 # ---------------------------------
@@ -25,9 +25,9 @@
 #SBATCH --job-name=a.chertkov_tetradat
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --time=0-30:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --partition gpu
-##SBATCH --mem-per-cpu=1500MB
+##SBATCH --mem-per-cpu=5000MB
 ##SBATCH --mem=5GB
 #SBATCH --mail-type=ALL
 #SBATCH --output=zhores_out.txt
@@ -41,8 +41,18 @@ module load gpu/cuda-12.0
 conda activate tetradat
 
 srun python3 manager.py --task check --kind data --data imagenet
+
 srun python3 manager.py --task check --kind model --data imagenet --model alexnet
 srun python3 manager.py --task check --kind model --data imagenet --model vgg16
 srun python3 manager.py --task check --kind model --data imagenet --model vgg19
+
+srun python3 manager.py --task attack --kind attr --data imagenet --model alexnet --model_attr vgg16
+srun python3 manager.py --task attack --kind attr --data imagenet --model alexnet --model_attr vgg19
+
+srun python3 manager.py --task attack --kind attr --data imagenet --model vgg16 --model_attr alexnet
+srun python3 manager.py --task attack --kind attr --data imagenet --model vgg16 --model_attr vgg19
+
+srun python3 manager.py --task attack --kind attr --data imagenet --model vgg19 --model_attr alexnet
+srun python3 manager.py --task attack --kind attr --data imagenet --model vgg19 --model_attr vgg16
 
 exit 0
