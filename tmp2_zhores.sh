94c6fa0ded36a3cbb704f8ddc6815cfcb409616b
AndreiChertkov <andre.chertkov@gmail.com>
2023-11-06 14:12:25 +0300

    Test

---
+++ tmp2_zhores.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=tet-2tmp
+#SBATCH --output=tmp2_zhores_out.txt
+#SBATCH --time=0-10:00:00
+#SBATCH --partition gpu
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --mem=30GB
+
+module rm *
+module load python/anaconda3
+module load gpu/cuda-11.3
+source activate tetradat
+conda activate tetradat
+
+srun python3 manager.py --task attack --kind attr --data imagenet --model mobilenet --model_attr alexnet --attack_num_max 10
+
+exit 0
