7c9a303831b00dcea6804e70989c4a2f87568df6
AndreiChertkov <andre.chertkov@gmail.com>
2024-05-14 16:28:42 +0300

    More accurate code and demo for similarity measure

---
+++ sim_wrapper.py
@@ -0,0 +1,20 @@
+from sentence_transformers import SentenceTransformer
+from sentence_transformers import util
+
+
+class SimWrapper:
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    def run(self, base, refs):
+        is_many = isinstance(refs, list)
+        if not is_many:
+            refs = [refs]
+
+        sentences = [base, refs]
+        embeddings = self.model.encode(sentences, convert_to_tensor=True)
+        scores = util.cos_sim(embeddings, embeddings)
+
+        scores = cosine_scores[0][1:]
+
+        return scores if is_many else scores[0]
--- test_sim.py
+++ test_sim.py
@@ -1,38 +1,23 @@
-from sentence_transformers import SentenceTransformer
-from sentence_transformers import util
+from time import perf_counter as tpc
 
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+from sim_wrapper import SimWrapper
 
 
-sentences = [
-    'The cat sits outside',
-    'A man is playing guitar',
-    'I love pasta',
-    'The new movie is awesome',
-    'The cat plays in the garden',
-    'A woman watches TV',
-    'The new movie is so great',
-    'Do you like pizza?',
-]
+if __name__ == '__main__':
+    t = tpc()
+    sim = SimWrapper()
+    print(f'\n\nPREPARED | Time: {tpc()-t:-8.2f} sec')
 
-# Compute embeddings
-embeddings = model.encode(sentences, convert_to_tensor=True)
+    t = tpc()
+    base = 'The cat sits outside'
+    ref = 'The cat sits here'
+    score = sim.run(base, ref)
+    print(f'\n\nDONE #1 | Time: {tpc()-t:-8.2f} sec | Result :\n', score)
 
-# Compute cosine-similarities for each sentence with each other sentence
-cosine_scores = util.cos_sim(embeddings, embeddings)
 
-# Find the pairs with the highest cosine similarity scores
-pairs = []
-for i in range(cosine_scores.shape[0]):
-    for j in range(cosine_scores.shape[1]):
-        pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
-
-# Sort scores in decreasing order
-pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
-
-for pair in pairs[0:10]:
-    i, j = pair['index']
-    print('{} \t\t {} \t\t Score: {:.4f}'.format(
-        sentences[i], sentences[j], pair['score']
-    ))
+    t = tpc()
+    base = 'The cat sits outside'
+    refs = ['The cat sits here', 'The cat sits at home', 'The cat sits outside']
+    scores = sim.run(base, refs)
+    print(f'\n\nDONE #2 | Time: {tpc()-t:-8.2f} sec | Result :\n', scores)
