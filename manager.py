e4f13ed120d77d23be16881c866f79c301ff4a01
AndreiChertkov <andre.chertkov@gmail.com>
2024-08-03 22:58:03 +0300

    Fix

--- manager.py
+++ manager.py
@@ -481,7 +481,7 @@ class Manager:
             fpath=self.get_path(f'img/{c}/base.png'))
 
         x = self.data.tr_norm_inv(x)
-        x = style.run(image, self.style_prompt)
+        x = style.run(x, self.style_prompt)
 
         self.data.plot_base(self.data.tr_norm_inv(x), '', size=6,
             fpath=self.get_path(f'img/{c}/base_style.png'))
